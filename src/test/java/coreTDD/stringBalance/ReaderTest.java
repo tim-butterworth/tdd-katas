1f01578e0ef8617b4412845975e9205d579b2c6a
Tim <timothy.d.butterworth@gmail.com>
2018-10-30 07:18:57 -0600

    Changes to string balance test to be a parameterized test

--- src/test/java/coreTDD/stringBalance/ReaderTest.java
+++ src/test/java/coreTDD/stringBalance/ReaderTest.java
@@ -1,13 +1,37 @@
 package coreTDD.stringBalance;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(Parameterized.class)
 public class ReaderTest {
 
+    private final Reader reader = new Reader();
+
+    private final String message;
+    private final String stringToTest;
+    private final Boolean expected;
+
+    public ReaderTest(String message, String stringToTest, Boolean expected) {
+        this.message = message;
+        this.stringToTest = stringToTest;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] parameters() {
+        return new Object[][]{
+                {"empty string is balanced", "", true},
+        };
+    }
+
     @Test
-    public void garbageTest() {
-        assertThat(true).isFalse();
+    public void testWithParameters() {
+        assertThat(reader.isBalanced(stringToTest))
+                .as(message)
+                .isEqualTo(expected);
     }
 }
