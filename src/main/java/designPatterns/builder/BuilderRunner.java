c93a2ed6e99499bbc71d9360f77169afc060f1eb
Tim <timothy.d.butterworth@gmail.com>
2019-02-20 08:00:11 -0700

    Removes immutable value dependencies and usages because it prevents the project from building on THE network

--- src/main/java/designPatterns/builder/BuilderRunner.java
+++ src/main/java/designPatterns/builder/BuilderRunner.java
@@ -1,14 +1,10 @@
 package designPatterns.builder;
 
-import designPatterns.builder.notRequiredProperties.ImmutableNoPropsRequiredGenerated;
 import designPatterns.builder.notRequiredProperties.NoPropsRequired;
-import designPatterns.builder.notRequiredProperties.NoPropsRequiredGenerated;
 import designPatterns.builder.reflection.BuilderForSampleObj;
 import designPatterns.builder.reflection.SampleObj;
 import designPatterns.builder.reflectionBasedProxy.ReflectionBuilderFactory;
 import designPatterns.builder.requiredProperties.AllPropsRequired;
-import designPatterns.builder.requiredProperties.AllPropsRequiredGenerated;
-import designPatterns.builder.requiredProperties.ImmutableAllPropsRequiredGenerated;
 
 import java.lang.reflect.Method;
 
@@ -19,21 +15,7 @@ public class BuilderRunner {
                 .name("name")
                 .build();
 
-        AllPropsRequiredGenerated someName = ImmutableAllPropsRequiredGenerated.builder()
-                .age(12)
-                .name("someName")
-                .build();
-
-        AllPropsRequiredGenerated build = ImmutableAllPropsRequiredGenerated.builder().from(someName).build();
-
-        System.out.println(build == someName);
-        System.out.println(build.equals(someName));
         NoPropsRequired noPropsRequired = NoPropsRequired.builder().build();
-
-        NoPropsRequiredGenerated noPropsRequiredGenerated = ImmutableNoPropsRequiredGenerated.builder().build();
-
-        System.out.println(noPropsRequiredGenerated.getAge());
-
 
         ReflectionBuilderFactory reflectionBuilderFactory = new ReflectionBuilderFactory();
 
--- src/main/java/designPatterns/builder/requiredProperties/AllPropsRequiredGenerated.java
+++
@@ -1,9 +0,0 @@
-package designPatterns.builder.requiredProperties;
-
-import org.immutables.value.Value;
-
-@Value.Immutable
-public interface AllPropsRequiredGenerated {
-    String getName();
-    Integer getAge();
-}
